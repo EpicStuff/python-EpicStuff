c779ee6e6e30228995d9089a06ce2604f817cb61
EpicStuff <EpicStuff@users.noreply.github.com>
2024-03-11 09:56:16 -0400

    added/updated recursive_convert for Dict to support lists

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ a few somewhat useful python objects
 -   [ ] when doing bar in bar with the second bar being transient, make so that the dots continue from where the previous bar left off
 -   [ ] implement auto transient for bar in bar
 -   [ ] add and implement simple=False for .Bar.track()
+
+## Stuff:
+
+-   Note to self: build by running `python -m build` then `twine upload dist/*`
--- src/EpicStuff/dict.py
+++ src/EpicStuff/dict.py
@@ -3,15 +3,34 @@ from typing import Any
 class Dict(dict):
 	'''
 	The class gives access to the dictionary through the attribute name.
-	basically copied (then simplified) from https://github.com/bstlabs/py-jdict
+	inspired by https://github.com/bstlabs/py-jdict and https://github.com/cdgriffith/Box
 	'''
-	def __init__(self, *args, **kwargs) -> None:
+	def __init__(self, *args, recursive_convert=True, **kwargs) -> None:
+		def handle_list(value: list) -> list:
+			'''
+			runs handle_dict on all (nested) elements
+			:param value: list
+			:return: list
+			'''
+			#  if value is a list, run it though this function
+			if isinstance(value, list):
+				value = list(map(handle_list, value))
+			return handle_dict(value)
+		def handle_dict(value: dict) -> dict:
+			'''
+			converts (nested) dicts to Dicts
+			:param value: dict
+			:return: dict
+			'''
+			if isinstance(value, dict):
+				value = Dict(value)
+			return value
+
 		super().__init__(*args, **kwargs)
 		# convert nested dicts to Dicts
-		for key, value in self.items():
-			# TODO: maybe handle self reference
-			if isinstance(value, dict):
-				self[key] = Dict(value)
+		if recursive_convert:
+			for key, value in self.items():
+				self[key] = handle_list(value)
 	def __getattr__(self, key: str) -> Any:
 		'''
 		Method returns the value of the named attribute of an object. If not found, it returns null object.
