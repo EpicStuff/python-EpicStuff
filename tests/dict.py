a74ff9cb3da0d69f183403f159dc26b326ce16de
EpicStuff <EpicStuff@users.noreply.github.com>
2024-03-13 11:45:51 -0400

    simplified things and fixed a few things i think

--- tests/dict.py
+++ tests/dict.py
@@ -1,10 +1,38 @@
 from epicstuff import Dict
 from box import Box
 
-x = Dict({'a': 1, 'b': {'c': 2, 'd': [3, [[dict(), 3], {'a': dict()}, dict()], {'e': 5}]}}, _convert=False)
+print('starting')
+
+x = Dict({'a': 1, 'b': {'c': [3, dict()]}}, _convert=False)
 
 print(x.b)
 
+x.__contains__('a')
+
+'a' in x
+
+print(len(x))
+
+print(x.copy())
+
+x.t = 1
+
+x._w = 2
+
+print(x._w)
+
+print(*[x for x in x])
+
+x['f'] = 'g'
+
+print(x['f'])
+
+print(x == 3)
+
+print(x | {'a': 2, 'e': 999})
+print({'a': 2, 'e': 999} | x)
+
+print(reversed(x))
 
 class child(Dict):
 	def __init__(self, *args, **kwargs) -> None:
@@ -14,11 +42,11 @@ class child(Dict):
 
 y = child(w=5)
 
-z = Dict()
+# z = Dict()
 
-y.y = 6
+# y.y = 6
 
-print(y.y)
-print(y)
-print('---')
-print(y.w)
+# print(y.y)
+# print(y)
+# print('---')
+# print(y.w)
