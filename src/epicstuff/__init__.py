1471c6587184212b939f8b7bcaeea458f880cd7c
EpicStuff <EpicStuff@users.noreply.github.com>
2024-09-05 13:24:19 -0400

    added open and wrap

--- src/epicstuff/__init__.py
+++ src/epicstuff/__init__.py
@@ -2,10 +2,13 @@ from .progress import Bar
 from .dict import Dict
 from .timer import timer
 from .version import __version__
+from .stuff import *  # pylint: disable=redefined-builtin
 
 __all__ = [
 	'Bar',
 	'Dict',
 	'timer',
+	'wrap',
+	'open',
 	'__version__',
 ]
