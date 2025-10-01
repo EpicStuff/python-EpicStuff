ca1de2dedaedeec5dacd7a2a79aadd92fa74ce09
EpicStuff <EpicStuff@users.noreply.github.com>
2025-10-01 16:46:56 -0400

    updated dict

--- tests/dict.py
+++ tests/dict.py
@@ -1,14 +1,17 @@
+# ruff: noqa: S101
+
 from epicstuff import Dict
-from box import Box
 
 print('starting')
 
-x = Dict({'a': 1, 'b': {'c': [3, dict()]}}, _convert=False)
+convert = True
+x = Dict({'a': 1, 'b': {'c': [3, dict()]}}, _convert=convert)
 
 assert x.b == {'c': [3, dict()]}
 
 assert 'a' in x
 
+print('len:', len(x))
 assert len(x) == 2
 
 assert x.copy() is not x
@@ -19,7 +22,10 @@ x._w = 2
 
 assert x._w == 2
 
-assert [x for x in x] == ['a', 'b', 't']
+print(x)
+
+# assert [x for x in x] == ['a', 'b', 't']  # not sure how i want to treat attributes that start with
+assert [x for x in x] == ['a', 'b', 't', '_w']
 
 x['f'] = 'g'
 
@@ -34,11 +40,13 @@ print(reversed(x))
 
 class child(Dict):
 	def __init__(self, *args, **kwargs) -> None:
+		self._convert = True
 		print('initing')
 		self.x = 3
 
 
 y = child(w=5)
+print(y)
 
 # z = Dict()
 
