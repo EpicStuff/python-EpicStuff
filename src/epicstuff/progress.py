31b5d939b624fc2d7debd61f9da9ce1fa498a356
EpicStuff <EpicStuff@users.noreply.github.com>
2026-03-25 11:32:33 -0400

    updated progress to add minor features

--- src/epicstuff/progress.py
+++ src/epicstuff/progress.py
@@ -1,8 +1,6 @@
+import itertools, threading, time
 from collections.abc import Callable, Iterable, Sequence
-from itertools import cycle
 from operator import length_hint
-from threading import Thread
-from time import sleep
 from types import TracebackType
 
 from rich.progress import MofNCompleteColumn, Progress, ProgressType, SpinnerColumn, TaskID, TimeElapsedColumn
@@ -19,28 +17,36 @@ class Bar:
 
 	'''
 
-	def __init__(self, auto_refresh: bool = True, refresh_per_second: float = 10, speed_estimate_period: float = 30.0, expand: bool = False) -> None:
+	def __init__(self, auto_refresh: bool = True, refresh_per_second: float = 10, speed_estimate_period: float = 30.0, expand: bool = False, add_columns: dict[int, Callable] | None = None, remove_columns: list[int] | None = None) -> None:
+		self.tasks = []
 		self.progress = None
 		self.auto_refresh = auto_refresh
 		self.refresh_per_second = refresh_per_second
 		self.speed_estimate_period = speed_estimate_period
 		self.expand = expand
+		self.add_columns = add_columns or {}
+		self.remove_columns = remove_columns or []
 	def __enter__(self) -> Callable:
-		self.progress = Progress(MofNCompleteColumn(), SpinnerColumn(), *Progress.get_default_columns(), TimeElapsedColumn(), auto_refresh=self.auto_refresh, refresh_per_second=self.refresh_per_second, speed_estimate_period=self.speed_estimate_period, expand=self.expand)
+		columns = [MofNCompleteColumn(), SpinnerColumn(), *Progress.get_default_columns(), TimeElapsedColumn()]
+		for index, column in self.add_columns.items():
+			columns.insert(index, column)
+		for index in self.remove_columns:
+			del columns[index]
+		self.progress = Progress(*columns, auto_refresh=self.auto_refresh, refresh_per_second=self.refresh_per_second, speed_estimate_period=self.speed_estimate_period, expand=self.expand)
 		self.progress.start()
 		return self.track
 	def __exit__(self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None) -> None:
 		self.progress.stop()
 	def _cycle(self, desc: str, task: TaskID, delay: float = 0.5) -> None:
 		'Cycle description through ..., , ., and ..'
-		desc_s = cycle([f'{desc}   ', f'{desc}.  ', f'{desc}.. ', f'{desc}...'])
+		desc_s = itertools.cycle([f'{desc}   ', f'{desc}.  ', f'{desc}.. ', f'{desc}...'])
 		# while task is ongoing
 		while task in self.progress.task_ids:
 			# cycle the description to the next one
 			self.progress.update(task, description=next(desc_s))
 			# wait delay seconds
-			sleep(delay)
-	def track(self, sequence: Iterable[ProgressType] | Sequence[ProgressType], description: str = "Working", total: float | None = None, transient: bool = False) -> Iterable[ProgressType]:
+			time.sleep(delay)
+	def track(self, sequence: Iterable[ProgressType] | Sequence[ProgressType], description: str = 'Working', total: float | None = -1, transient: bool = False, cycle: bool = True) -> Iterable[ProgressType]:
 		'''Track progress by iterating over a sequence.
 
 		Args:
@@ -48,13 +54,16 @@ class Bar:
 			description: (str, optional): Description of task, if new task is created.
 			total: (float, optional): Total number of steps. Default is len(sequence).
 			transient: (bool, optional): Clear the progress on exit. Defaults to False.
+			cycle: (bool, optional): Whether to cycle the description with dots while the task is ongoing. Defaults to True.
 
 		Returns:
 			Iterable[ProgressType]: An iterable of values taken from the provided sequence.
 
 		'''
-		task_id = self.progress.add_task(description, total=total or float(length_hint(sequence)) or None)
-		Thread(target=self._cycle, args=(description, task_id), daemon=True).start()
+		task_id = self.progress.add_task(description, total=total if total != -1 else float(length_hint(sequence)) or None)
+		self.tasks.append(task_id)
+		if cycle:
+			threading.Thread(target=self._cycle, args=(description, task_id), daemon=True).start()
 
 		for value in sequence:
 			yield value
